413527bd57081dc6d4bd6599be50b787c70fcf49
Tvishaasaraf1 <75128383+Tvishaasaraf1@users.noreply.github.com>
2020-12-28 16:55:42 +0530

    Add files via upload

---
+++ log.js
@@ -0,0 +1,24 @@
+class Log {
+    constructor(x,y,height,angle){
+        var options = {
+            restitution:0.1,
+            friction: 2,
+            density: 2
+        }
+        this.body = Bodies.rectangle(x,y,20,height,options) 
+        this.width = 20
+        this.height = height
+        Matter.Body.setAngle(this.body,angle)
+        World.add(world,this.body)
+    }
+    display(){
+        push()
+        translate(this.body.position.x,this.body.position.y)
+        rotate(this.body.angle)
+        fill("Pink")
+        rectMode(CENTER)
+        rect(0,0,this.width,this.height) 
+        pop()
+        
+    }
+}
